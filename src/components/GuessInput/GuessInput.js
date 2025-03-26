3ce9cdca922346998b583b9687f003b99dffdae1
MDProjectMT <150595288+MDProjectMT@users.noreply.github.com>
2025-03-26 12:09:41 +0100

    commit InputGuess exercise 1
    exercise 1

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
 
 function GuessInput() {
-  return <div></div>;
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
+
+  function handleSubmit(ev) {
+    ev.preventDefault();
+    setTentativeGuess(tentativeGuess);
+    console.log({ tentativeGuess });
+    setTentativeGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        value={tentativeGuess}
+        onChange={(ev) => {
+          const nextGuess = ev.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
