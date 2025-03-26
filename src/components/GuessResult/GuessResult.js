4d19a725345e458d3452f6743d17b646077c9cc8
MDProjectMT <150595288+MDProjectMT@users.noreply.github.com>
2025-03-26 14:46:09 +0100

    commit Guess exercise 3
    exercise 3

--- src/components/GuessResult/GuessResult.js
+++ src/components/GuessResult/GuessResult.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Guess from "../Guess/Guess";
 
 function GuessResult({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p key={index}>{guess}</p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses ? guesses[num] : undefined} />
       ))}
     </div>
   );
 }
 
 export default GuessResult;
+// {guesses.map((guess, index) => (
+//       <p key={index}>{guess}</p>
+//   ))}
